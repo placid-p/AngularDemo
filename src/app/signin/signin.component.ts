92205a58f42cb783d0e1ae6d912b7f9793defc6b
Shashikant Chandge <purva.shashikant-chandge@capgemini.com>
2023-02-23 13:34:28 +0530

    Backend code added

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import{HttpClient}from '@angular/common/http';
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
-
-  constructor() { }
+  constructor(private http:HttpClient){}
+  Onsubmit(data:any){
+    this.http.post('http://localhost:8081/addperson',data)
+    .subscribe((result)=>{
+      console.warn(result);
+    })
+    console.warn(data);
+  }
+ 
 
   ngOnInit(): void {
   }
